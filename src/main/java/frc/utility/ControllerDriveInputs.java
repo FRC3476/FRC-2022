7c84a2b4d24c4b7c732bb94a37a1687cc00c9b93
varun7654 <59785640+varun7654@users.noreply.github.com>
2021-11-27 12:41:11 -0800

    make edge of snapping deadzone get scaled

--- src/main/java/frc/utility/ControllerDriveInputs.java
+++ src/main/java/frc/utility/ControllerDriveInputs.java
@@ -26,8 +26,10 @@ public class ControllerDriveInputs {
      * Apply a circular deadzone on the controller and then scale the joystick values
      * @param snappingDeadZoneX An X value smaller than this value will get set to 0 
      * (This help you with driving straight forwards and backwards) 
+     * Larger x values will be scaled so that the edge of the deadzone will be 0
      * @param snappingDeadZoneY An Y value smaller than this value will get set to 0 
      * (This help you with driving straight sideways)
+     * Larger y values will be scaled so that the edge of the deadzone will be 0
      * @param rotationDeadZone A rotation value smaller than this value will get set to 0. 
      * Larger rotation values will be scaled so that the edge of the deadzone will be 0
      * @param circularDeadZone If the controller input is inside the circle with a radius of 
@@ -36,10 +38,6 @@ public class ControllerDriveInputs {
      * @return {@link ControllerDriveInputs} 
      */
     public ControllerDriveInputs applyDeadZone(double snappingDeadZoneX, double snappingDeadZoneY, double rotationDeadZone, double circularDeadZone){
-        if(Math.abs(x)<snappingDeadZoneX) x = 0;
-        if(Math.abs(y)<snappingDeadZoneY) y = 0;
-        if(Math.abs(rotation)<rotationDeadZone) rotation = 0;
-
 		double amplitudeSquared = x*x + y*y;
 		if(amplitudeSquared<circularDeadZone*circularDeadZone){
 			x = 0;
@@ -53,8 +51,16 @@ public class ControllerDriveInputs {
 
 			x = Math.copySign(OrangeUtility.coercedNormalize(Math.abs(x), Math.abs(minx), 1, 0, 1), x);
 			y = Math.copySign(OrangeUtility.coercedNormalize(Math.abs(y), Math.abs(miny), 1, 0, 1), y);
-		}
-		rotation = Math.copySign(OrangeUtility.coercedNormalize(Math.abs(rotation), Math.abs(rotationDeadZone), 1, 0, 1), rotation);
+        }
+
+        //coercedNormalize should do this for us
+        // if(Math.abs(x)<snappingDeadZoneX) x = 0;
+        // if(Math.abs(y)<snappingDeadZoneY) y = 0;
+        // if(Math.abs(rotation)<rotationDeadZone) rotation = 0;
+
+        rotation = Math.copySign(OrangeUtility.coercedNormalize(Math.abs(rotation), Math.abs(rotationDeadZone), 1, 0, 1), rotation);
+        x = Math.copySign(OrangeUtility.coercedNormalize(Math.abs(x), Math.abs(snappingDeadZoneX), 1, 0, 1), x);
+        y = Math.copySign(OrangeUtility.coercedNormalize(Math.abs(y), Math.abs(snappingDeadZoneY), 1, 0, 1), y);
 
         return this;
     }
