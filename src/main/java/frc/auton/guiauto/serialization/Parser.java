5fa22cf53fb41b35704f15b5576a2344b05e3b4f
varun7654 <59785640+varun7654@users.noreply.github.com>
2021-11-28 15:39:04 -0800

    redesign the parser for auto

--- src/main/java/frc/auton/guiauto/serialization/Parser.java
+++ src/main/java/frc/auton/guiauto/serialization/Parser.java
@@ -6,49 +6,33 @@ import frc.utility.OrangeUtility;
 public class Parser {
     //Again the TemplateAuto method argument is not necessary. We use it to access methods in our superclass of our auto
     public static boolean execute(String string, TemplateAuto context){
-        String[] commands = string.split("\n"); //Start by spliting everything by lines (commands)
+        String[] commands = string.split("\n"); //Start by splitting everything by lines (commands)
         for (String command : commands) {
-            StringBuilder methodName = null;
-            StringBuilder argument = null;
-            for (int i = 0; i < command.length(); i++) { //Loop though all the characters
-                char character = command.charAt(i);
-                if(argument != null){
-                    //We've reached the argument part of the command. Everything else is part of the argument
-                    argument.append(character);
-                } else if(methodName == null){
-                    //We haven't initalized the beginning of the methodName part of the command yet
-                    //This means we haven't seen the first character of the method name yet
-                    if(!Character.isWhitespace(character)){
-                        //If we see the first character initalize the method name add add the character
-                        methodName = new StringBuilder().append(character);
-                    } //else do nothing and continue looping until we find our first character
-                } else if(!Character.isWhitespace(character)){
-                    // Keep adding to the method name until we hit whitespace
-                    methodName.append(character);
-                } else {
-                    //We've hit section of whitespace directly before the argument part of the command
-                    //Initalize the argument to record that we've reached this section and skip the first whitespace
-                    argument = new StringBuilder();
+            if(command.startsWith("#")) continue; //Ignore comments
+            String[] parts = command.split(" "); //Split by spaces
+            if(parts.length == 0) continue; //Ignore empty lines
+            String method = parts[0]; //Get the method name
+            String[] args = new String[parts.length - 1]; //Initialize the argument array
+            System.arraycopy(parts, 1, args, 0, parts.length - 1);
+            try {
+                switch (method) {
+                    case "print":
+                        StringBuilder aggregate = new StringBuilder();
+                        for (String arg : args) {
+                            aggregate.append(arg).append(" ");
+                        }
+                        System.out.println(aggregate);
+                        break;
+                    case "sleep":
+                        OrangeUtility.sleep(Long.parseLong(args[0]));
+                        break;
+                    //Add more cases for other robot functions
+                    default:
+                        return false; //Command does not exist
                 }
+            } catch (Exception e) {
+                return false;
             }
-
-            //System.out.println("Command " + command + " method name: " + methodName + " argument: " + argument);
-            if(methodName != null){ //Check that we don't have a blank command/methodName
-                try{    
-                    //Figure out what the command is and execute it
-                    switch (methodName.toString()){
-                        case "print":
-                            System.out.println(argument);
-                            break;
-                        case "sleep":
-                                OrangeUtility.sleep(Long.parseLong(argument.toString()));
-                            break;
-                        default:
-                            return false; 
-                    }
-                } catch (NumberFormatException | NullPointerException e) { return false; } 
-            } //else and continue to the next command (or auto step)
-            
         }
         return true;
     }
