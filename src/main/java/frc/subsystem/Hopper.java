46566e1dababb0d661eb882f8d0b67d2053d2c9b
Aditya Rawat <aditya1rawat@gmail.com>
2022-01-23 16:17:33 -0800

    new: still todo but added "accurate" color detection

--- src/main/java/frc/subsystem/Hopper.java
+++ src/main/java/frc/subsystem/Hopper.java
@@ -1,6 +1,7 @@
 package frc.subsystem;
 
 import com.revrobotics.CANSparkMaxLowLevel;
+import com.revrobotics.ColorMatch;
 import com.revrobotics.ColorSensorV3;
 import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -14,6 +15,20 @@ public class Hopper extends AbstractSubsystem {
     LazyCANSparkMax hopperMotor;
     private final ColorSensorV3 colorSensor = new ColorSensorV3(I2C.Port.kMXP);
 
+    /**
+     * A Rev Color Match object is used to register and detect known colors. This can be calibrated ahead of time or during
+     * operation.
+     * <p>
+     * This object uses a simple euclidian distance to estimate the closest match with given confidence range.
+     */
+    private final ColorMatch colorMatcher = new ColorMatch();
+
+    /**
+     * Note: Any example colors should be calibrated as the user needs, these are here as a basic example.
+     */
+    private final Color blueTarget = new Color(0.184, 0.427, 0.39);
+    private final Color redTarget = new Color(0.38, 0.41, 0.2);
+
     public static Hopper getInstance() {
         return instance;
     }
@@ -25,6 +40,8 @@ public class Hopper extends AbstractSubsystem {
     private Hopper() {
         super(Constants.HOPPER_PERIOD);
         hopperMotor = new LazyCANSparkMax(Constants.HOPPER_MOTOR_ID, CANSparkMaxLowLevel.MotorType.kBrushless);
+        colorMatcher.addColorMatch(blueTarget);
+        colorMatcher.addColorMatch(redTarget);
     }
 
     public void setHopperState(HopperState hopperState) {
@@ -44,10 +61,18 @@ public class Hopper extends AbstractSubsystem {
     @Override
     public void update() {
         Color detectedColor = colorSensor.getColor();
-        private final Color kBlueTarget = ColorMatch.makeColor(0.143, 0.427, 0.429);
         SmartDashboard.putNumber("Red", detectedColor.red);
         SmartDashboard.putNumber("Green", detectedColor.green);
         SmartDashboard.putNumber("Blue", detectedColor.blue);
+        SmartDashboard.putNumber("Confidence", colorMatcher.matchClosestColor(detectedColor).confidence);
+
+        if (colorMatcher.matchClosestColor(detectedColor).color == redTarget) {
+            SmartDashboard.putString("Color", "Red");
+        } else if (colorMatcher.matchClosestColor(detectedColor).color == blueTarget) {
+            SmartDashboard.putString("Color", "Blue");
+        }
+//        SmartDashboard.putNumber("Raw Color", Double.parseDouble(colorSensor.getRawColor().toString()));
+//        SmartDashboard.putNumber("Closest Detected Color", Double.parseDouble(detectedColor.toString()));
     }
 
     @Override
