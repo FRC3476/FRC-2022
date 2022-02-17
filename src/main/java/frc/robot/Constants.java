0f29f1058c275783ce01e12d6d15199207f1568c
varun7654 <varun7654@outlook.com>
2022-02-16 17:46:24 -0800

    style: fix comment in allowed hood angle error

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -287,8 +287,7 @@ public final class Constants {
     public static final double HOMING_MOTOR_PERCENT_OUTPUT = 0.30;
 
     /**
-     * Allowed error when comparing Hood angle to a desired angle Units are in rotations of the motor. 1 Rotation is 3.69230
-     * degrees of the hood
+     * Allowed error when comparing Hood angle to a desired angle Units are in degrees
      */
     public static final double ALLOWED_HOOD_ANGLE_ERROR = 1;
 
