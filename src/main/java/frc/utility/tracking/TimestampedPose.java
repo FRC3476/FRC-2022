8af91e42d599f33d6ffc1b3cc1e8e6653a752aa1
varun7654 <varun7654@outlook.com>
2022-04-13 18:31:13 -0700

    add equals and hashcode to TimestampedPose

--- src/main/java/frc/utility/tracking/TimestampedPose.java
+++ src/main/java/frc/utility/tracking/TimestampedPose.java
@@ -3,16 +3,31 @@ package frc.utility.tracking;
 import edu.wpi.first.math.geometry.Pose2d;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Objects;
+
 
 public final class TimestampedPose implements Comparable<TimestampedPose> {
     public final double timestamp;
-    public final Pose2d pose;
+    public final @NotNull Pose2d pose;
 
-    public TimestampedPose(double timestamp, Pose2d pose) {
+    public TimestampedPose(double timestamp, @NotNull Pose2d pose) {
         this.timestamp = timestamp;
         this.pose = pose;
     }
 
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        TimestampedPose that = (TimestampedPose) obj;
+        return Double.compare(that.timestamp, timestamp) == 0 && Objects.equals(pose, that.pose);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(timestamp, pose);
+    }
 
     @Override
     public int compareTo(@NotNull TimestampedPose o) {
