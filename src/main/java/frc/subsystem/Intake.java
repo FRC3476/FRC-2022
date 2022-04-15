710c8fd9e091653a25fb03d80adba9a6798d7d18
varun7654 <varun7654@outlook.com>
2022-04-15 14:24:48 -0700

    update intake to falcon

--- src/main/java/frc/subsystem/Intake.java
+++ src/main/java/frc/subsystem/Intake.java
@@ -1,14 +1,15 @@
 package frc.subsystem;
 
-import com.revrobotics.CANSparkMaxLowLevel;
-import com.revrobotics.CANSparkMaxLowLevel.PeriodicFrame;
+import com.ctre.phoenix.motorcontrol.ControlFrame;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.StatusFrameEnhanced;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.Constants;
 import frc.subsystem.Hopper.OuttakeState;
 import frc.utility.OrangeUtility;
 import frc.utility.Timer;
-import frc.utility.controllers.LazyCANSparkMax;
 
 import static frc.utility.Pneumatics.getPneumaticsHub;
 
@@ -17,7 +18,7 @@ public final class Intake extends AbstractSubsystem {
     private static Intake instance = new Intake();
 
     private final Solenoid intakeSol;
-    private final LazyCANSparkMax intakeMotor;
+    private final TalonFX intakeMotor;
 
     private double allowIntakeRunTime = Double.MAX_VALUE;
 
@@ -28,12 +29,14 @@ public final class Intake extends AbstractSubsystem {
     private Intake() {
         super(Constants.INTAKE_PERIOD, 4);
         intakeSol = getPneumaticsHub().makeSolenoid(Constants.INTAKE_SOLENOID_CHANNEL);
-        intakeMotor = new LazyCANSparkMax(Constants.INTAKE_MOTOR_DEVICE_ID, CANSparkMaxLowLevel.MotorType.kBrushless);
-        intakeMotor.setPeriodicFramePeriod(PeriodicFrame.kStatus0, 100);
-        intakeMotor.setPeriodicFramePeriod(PeriodicFrame.kStatus1, 500);
-        intakeMotor.setPeriodicFramePeriod(PeriodicFrame.kStatus2, 500);
-        intakeMotor.setSmartCurrentLimit(30);
-        intakeMotor.setControlFramePeriodMs(25);
+        intakeMotor = new TalonFX(Constants.INTAKE_MOTOR_DEVICE_ID);
+
+        intakeMotor.setStatusFramePeriod(StatusFrameEnhanced.Status_1_General, 97); // Default is 10ms
+        intakeMotor.setStatusFramePeriod(StatusFrameEnhanced.Status_2_Feedback0, 101); // Default is 10ms
+        intakeMotor.setStatusFramePeriod(StatusFrameEnhanced.Status_Brushless_Current, 103); // Default is 50ms
+        intakeMotor.setControlFramePeriod(ControlFrame.Control_3_General, 23);
+        intakeMotor.setControlFramePeriod(ControlFrame.Control_4_Advanced, 29);
+        intakeMotor.setControlFramePeriod(ControlFrame.Control_6_MotProfAddTrajPoint, 547);
     }
 
     @Override
@@ -48,8 +51,8 @@ public final class Intake extends AbstractSubsystem {
 
     @Override
     public void logData() {
-        SmartDashboard.putNumber("Intake Motor Speed: ", intakeMotor.get());
-        SmartDashboard.putNumber("Intake Current", intakeMotor.getOutputCurrent());
+        SmartDashboard.putNumber("Intake Motor Speed: ", intakeMotor.getMotorOutputPercent());
+        SmartDashboard.putNumber("Intake Current", intakeMotor.getStatorCurrent());
         SmartDashboard.putBoolean("Intake Solenoid State: ", intakeSol.get());
         logData("Wanted Intake State", wantedIntakeState);
     }
@@ -58,7 +61,6 @@ public final class Intake extends AbstractSubsystem {
     @Override
     public void close() throws Exception {
         intakeSol.close();
-        intakeMotor.close();
         instance = new Intake();
     }
 
@@ -102,18 +104,18 @@ public final class Intake extends AbstractSubsystem {
         switch (intakeState) {
             case INTAKE:
                 if (Hopper.getInstance().getOuttakeState() == OuttakeState.AUTO_EJECT) {
-                    intakeMotor.set(Constants.INTAKE_EJECTION_SPEED);
+                    intakeMotor.set(ControlMode.PercentOutput, Constants.INTAKE_EJECTION_SPEED);
                 } else {
-                    intakeMotor.set(Constants.INTAKE_SPEED);
+                    intakeMotor.set(ControlMode.PercentOutput, Constants.INTAKE_SPEED);
                 }
                 break;
 
             case EJECT:
-                intakeMotor.set(-Constants.INTAKE_SPEED);
+                intakeMotor.set(ControlMode.PercentOutput, -Constants.INTAKE_SPEED);
                 break;
 
             case OFF:
-                intakeMotor.set(0);
+                intakeMotor.set(ControlMode.PercentOutput, 0);
                 break;
         }
     }
