1259748744e7b6ab49f64dfba38f38684864b7f6
varun7654 <varun7654@outlook.com>
2022-04-15 14:25:31 -0700

    don't allow robot to move in test

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -855,6 +855,8 @@ public class Robot extends TimedRobot {
         final Intake intake = Intake.getInstance();
         final Shooter shooter = Shooter.getInstance();
 
+
+        drive.swerveDrive(new ChassisSpeeds(0, 0, 0));
         if (!Constants.GRAPPLE_CLIMB) {
             Climber climber = Climber.getInstance();
 
