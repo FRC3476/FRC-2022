57a85654bf29c778ecc35fdb13f905eb9d96fb04
varun7654 <varun7654@outlook.com>
2022-05-12 22:49:43 -0700

    decrease the number of itertations for the VisionManager Test

--- src/test/java/frc/subsystem/VisionManagerTest.java
+++ src/test/java/frc/subsystem/VisionManagerTest.java
@@ -14,7 +14,7 @@ class VisionManagerTest {
         int failures = 0;
 
         Random random = new Random(719479);
-        for (int i = 0; i < 10000000; i++) {
+        for (int i = 0; i < 100000; i++) {
             Translation2d goalPos = new Translation2d((random.nextDouble() * 20) - 10, (random.nextDouble() * 20) - 10);
             Translation2d robotVelocity = new Translation2d((random.nextDouble() * 10) - 5, (random.nextDouble() * 10) - 5);
             Translation2d fakeGoal = VisionManager.getInstance().getVelocityAdjustedRelativeTranslation(goalPos, robotVelocity);
