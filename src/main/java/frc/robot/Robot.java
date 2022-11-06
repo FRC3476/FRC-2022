a3b6a6c2a77317f033e65317309abb48c7e02316
varun7654 <varun7654@outlook.com>
2022-11-06 12:05:25 -0800

    disable going to robot relative mode

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -408,7 +408,7 @@ public class Robot extends TimedRobot {
             hopper.toggleEjectOverride();
         }
 
-        if (xbox.getRisingEdge(XboxButtons.LEFT_CLICK) || xbox.getRisingEdge(Controller.XboxButtons.START)) {
+        if (false && (xbox.getRisingEdge(XboxButtons.LEFT_CLICK) || xbox.getRisingEdge(Controller.XboxButtons.START))) {
             useFieldRelative = !useFieldRelative;
             System.out.println("Field relative: " + useFieldRelative);
             SmartDashboard.putBoolean("Field Relative Enabled", useFieldRelative);
